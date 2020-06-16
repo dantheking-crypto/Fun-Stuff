51fa37f73b8c22a5e85d0df015cc613621ee7426
dantheking-crypto <danielfangzhu@gmail.com>
2020-06-16 14:38:21 -0700

    Create insertion.cpp

---
+++ projects/sorting/insertion.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+inline void swap (int& a, int& b) {
+  int temp = a;
+  a = b;
+  b = temp;
+}
+void selectionsort (int arr[], int length) {
+  for (int i = 1; i < length; i++) {
+    int current = i;
+    while (current >= 0) {
+      if (arr[current] > arr[current - 1]) break;
+      swap (arr[current], arr[current - 1]);
+      current--;
+    }
+  }
+}
+int main() {
+  int length;
+  cin >> length;
+  int arr[length];
+  for (int i = 0; i < length; i++) {
+    cin >> arr[i];
+  }
+  selectionsort(arr, length);
+  for (int i = 0; i < length; i++) {
+    cout << arr[i] << " ";
+  }
+  return 0;
+}
